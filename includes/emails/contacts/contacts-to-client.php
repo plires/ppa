f980f6dec5f611b0efe7f4fd309bec0056ed3ead
Pablo Lires <pablolires@gmail.com>
2022-05-31 22:10:32 -0300

    Adecuacion de imagenes en plantillas de email

--- includes/emails/contacts/contacts-to-client.php
+++ includes/emails/contacts/contacts-to-client.php
@@ -315,7 +315,7 @@
           <tr>
               <td style="vertical-align: middle;">
                   
-                  <img src="https://ppabsas.com.ar/temporal/img/email/header-mail.jpg" style="margin:0; padding:0; border:none; display:block; width: 100%; height: auto;" border="0" alt="header" />
+                  <img src="https://ppabsas.com.ar/img/email/header-mail.jpg" style="margin:0; padding:0; border:none; display:block; width: 100%; height: auto;" border="0" alt="header" />
 
               </td>
           </tr>
@@ -401,11 +401,11 @@
               <td align="center"  style="background-color: #FFFFFF;padding: 10px;">
                 
                 <a target="_blank" rel="noopener noreferrer" href="{facebook}">
-                  <img src="https://ppabsas.com.ar/temporal/img/email/facebook.png" style="margin:0; margin-right: 20px; padding:0; border:none; display:inline-block; width: 16px; height: auto;" border="0" alt="facebook" />
+                  <img src="https://ppabsas.com.ar/img/email/facebook.png" style="margin:0; margin-right: 20px; padding:0; border:none; display:inline-block; width: 16px; height: auto;" border="0" alt="facebook" />
                 </a>
 
                 <a target="_blank" rel="noopener noreferrer" href="{instagram}">
-                  <img src="https://ppabsas.com.ar/temporal/img/email/instagram.png" style="margin:0; margin-right: 20px; padding:0; border:none; display:inline-block; width: 16px; height: auto;" border="0" alt="instagram" />
+                  <img src="https://ppabsas.com.ar/img/email/instagram.png" style="margin:0; margin-right: 20px; padding:0; border:none; display:inline-block; width: 16px; height: auto;" border="0" alt="instagram" />
                 </a>
 
               </td>
